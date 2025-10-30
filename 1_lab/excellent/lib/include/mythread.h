db41e9853f88eb415622564d1c31d90d195b7359
mashawarface <sony.sky.16@gmail.com>
2025-10-30 22:35:41 +0700

    done create_stack, mythread_create

--- 1_lab/excellent/lib/include/mythread.h
+++ 1_lab/excellent/lib/include/mythread.h
@@ -1,23 +1,36 @@
+#define PAGE 4096
+#define STACK_SIZE PAGE * 8
+
 #define mythread_cleanup_push(routine, arg)
 #define mythread_cleanup_pop(execute)
 
-typedef struct mythread_t {
-  unsigned long int tid;
+// typedef unsigned long int mythread_t;
+
+typedef void *(*start_routine_t)(void *);
+
+typedef struct mythread {
+  unsigned long int   thread_id;
+  start_routine_t     start_routine;
+  void*               args;
+  void*               retval;
+  void*               stack;
+  int                 stack_size;
+  int                 joined;
+  int                 exited;
 } mythread_t;
 
 typedef struct mythread_cleanup_buffer {
 } mythread_cleanup_buffer;
 
-int mythread_create(mythread_t newthread, void *(*routine)(void *),
-                    void *arguments);
+int mythread_create(mythread_t *thread, void *(*routine)(void *), void *args);
 
-void mythread_exit(void *thread_return);
+void mythread_exit(void *retval);
 
-mythread_t mythread_self(void);
+unsigned long int mythread_self(void);
 
 int mythread_equal(mythread_t thread1, mythread_t thread2);
 
-int mythread_join(mythread_t thread, void **thread_return);
+int mythread_join(mythread_t thread, void **retval);
 
 int mythread_detach(mythread_t thread);
 
--- 1_lab/excellent/lib/src/mythread.c
+++ 1_lab/excellent/lib/src/mythread.c
@@ -1,6 +1,76 @@
+#define _GNU_SOURCE
+#include <errno.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "/home/mary/NSU-OS/1_lab/excellent/lib/include/mythread.h"
 
-int mythread_create(mythread_t newthread, void *(*routine)(void *),
-                    void *arguments) {
-                        
-                    }
+void *create_stack(size_t size) {
+  void *stack;
+
+  stack = mmap(NULL, size, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS,
+               -1, 0);
+  if (stack == MAP_FAILED) {
+    printf("Mapping is failed, %s!\n", strerror(errno));
+    exit(1);
+  }
+
+  if (mprotect(stack, PAGE, PROT_NONE) == -1) {
+    printf("Changing protection is failed, %s!\n", strerror(errno));
+    exit(1);
+  }
+
+  return stack;
+}
+
+int mythread_startup(void *arg) {
+  puts("!");
+
+  mythread_t *thread = (mythread_t *)arg;
+
+  thread->retval = thread->start_routine(thread->args);
+  thread->exited = 1;
+
+  while (1) {
+    sleep(10);
+  }
+
+  //   return 0;
+}
+
+int mythread_create(mythread_t *tid, void *(*routine)(void *), void *args) {
+  void *stack;
+  size_t stack_size = STACK_SIZE - sizeof(mythread_t);
+  mythread_t *thread;
+
+  stack = create_stack(STACK_SIZE);
+
+  thread = (mythread_t *)(stack + stack_size);
+  thread->thread_id = (unsigned long int)thread;
+  thread->start_routine = routine;
+  thread->args = args;
+  thread->stack = stack;
+  thread->stack_size = stack_size + sizeof(mythread_t);
+  thread->joined = 0;
+  thread->exited = 0;
+
+  int child_pid;
+  child_pid = clone(mythread_startup, stack + stack_size,
+                    CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND |
+                        CLONE_THREAD | CLONE_SYSVSEM,
+                    (void *)thread);
+  if (child_pid == -1) {
+    printf("Clone is failed, %s!\n", strerror(errno));
+    return 1;
+  }
+
+  tid = thread;
+
+  return 0;
+}
