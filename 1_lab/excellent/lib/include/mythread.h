50e18c9f72c8c4e9511198414273c41dfce7705a
mashawarface <sony.sky.16@gmail.com>
2025-10-31 20:39:08 +0700

    done join with futexes

--- 1_lab/excellent/lib/include/mythread.h
+++ 1_lab/excellent/lib/include/mythread.h
@@ -4,20 +4,21 @@
 #define mythread_cleanup_push(routine, arg)
 #define mythread_cleanup_pop(execute)
 
-// typedef unsigned long int mythread_t;
-
 typedef void *(*start_routine_t)(void *);
 
 typedef struct mythread {
-  unsigned long int   thread_id;
-  start_routine_t     start_routine;
-  void*               args;
-  void*               retval;
-  void*               stack;
-  int                 stack_size;
-  int                 joined;
-  int                 exited;
-} mythread_t;
+  unsigned long int thread_id;
+  start_routine_t start_routine;
+  void *args;
+  void *retval;
+  void *stack;
+  int stack_size;
+  volatile int joined;
+  volatile int detached;
+  volatile int finished;
+} mythread_struct_t;
+
+typedef mythread_struct_t *mythread_t;
 
 typedef struct mythread_cleanup_buffer {
 } mythread_cleanup_buffer;
--- 1_lab/excellent/lib/src/mythread.c
+++ 1_lab/excellent/lib/src/mythread.c
@@ -1,15 +1,30 @@
 #define _GNU_SOURCE
+
+#include <asm-generic/errno-base.h>
+#include <err.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <linux/futex.h>
 #include <pthread.h>
+#include <stdatomic.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
+#include <sys/syscall.h>
+#include <sys/time.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "/home/mary/NSU-OS/1_lab/excellent/lib/include/mythread.h"
+
+static int futex(void *addr, int op, int val, const struct timespec *timeout,
+                 void *addr2, int val2) {
+  return syscall(SYS_futex, addr, op, val, timeout, addr2, val2);
+}
 
 void *create_stack(size_t size) {
   void *stack;
@@ -17,60 +32,116 @@ void *create_stack(size_t size) {
   stack = mmap(NULL, size, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS,
                -1, 0);
   if (stack == MAP_FAILED) {
-    printf("Mapping is failed, %s!\n", strerror(errno));
-    exit(1);
+    return NULL;
   }
 
   if (mprotect(stack, PAGE, PROT_NONE) == -1) {
-    printf("Changing protection is failed, %s!\n", strerror(errno));
-    exit(1);
+    munmap(stack, size);
+    return NULL;
   }
 
   return stack;
 }
 
-int mythread_startup(void *arg) {
-  puts("!");
-
-  mythread_t *thread = (mythread_t *)arg;
-
-  thread->retval = thread->start_routine(thread->args);
-  thread->exited = 1;
-
-  while (1) {
-    sleep(10);
+int destroy_thread(mythread_t thread) {
+  if (munmap(thread->stack, thread->stack_size) == -1) {
+    return errno;
   }
 
-  //   return 0;
+  return 0;
+}
+
+int mythread_startup(void *arg) {
+  mythread_t thread = (mythread_t)arg;
+
+  thread->retval = thread->start_routine(thread->args);
+  thread->finished = 1;
+
+  futex((unsigned long int *)&thread->finished, FUTEX_WAKE, 0, NULL, NULL, 0);
+
+  while (!thread->joined && !thread->detached) {
+    futex((void *)&thread->joined, FUTEX_WAIT, 0, NULL, NULL, 0);
+    futex((void *)&thread->detached, FUTEX_WAIT, 0, NULL, NULL, 0);
+  }
+
+  return 0;
+}
+
+int mythread_join(mythread_t thread, void **retval) {
+  if (thread == NULL) {
+    return ESRCH;
+  }
+
+  if (thread->detached) {
+    return EINVAL;
+  }
+
+  while (!thread->finished) {
+    futex((void *)&thread->finished, FUTEX_WAIT, 0, NULL, NULL, 0);
+  }
+
+  if (retval != NULL) {
+    *retval = thread->retval;
+  }
+
+  thread->joined = 1;
+
+  futex((void *)&thread->joined, FUTEX_WAKE, 0, NULL, NULL, 0);
+
+  destroy_thread(thread);
+
+  return 0;
+}
+
+int mythread_detach(mythread_t thread) {
+  if (!thread) {
+    return ESRCH;
+  }
+
+  if (thread->detached) {
+    return EINVAL;
+  }
+
+  thread->detached = 1;
+
+  futex((void *)&thread->detached, FUTEX_WAKE, 0, NULL, NULL, 0);
+
+  return 0;
 }
 
 int mythread_create(mythread_t *tid, void *(*routine)(void *), void *args) {
+  mythread_struct_t *thread;
   void *stack;
-  size_t stack_size = STACK_SIZE - sizeof(mythread_t);
-  mythread_t *thread;
 
   stack = create_stack(STACK_SIZE);
+  if (!stack) {
+    return errno;
+  }
 
-  thread = (mythread_t *)(stack + stack_size);
+  thread =
+      (mythread_struct_t *)(stack + STACK_SIZE - sizeof(mythread_struct_t));
   thread->thread_id = (unsigned long int)thread;
   thread->start_routine = routine;
   thread->args = args;
+  thread->retval = NULL;
   thread->stack = stack;
-  thread->stack_size = stack_size + sizeof(mythread_t);
+  thread->stack_size = STACK_SIZE;
   thread->joined = 0;
-  thread->exited = 0;
+  thread->detached = 0;
+  thread->finished = 0;
 
-  int child_pid;
-  child_pid = clone(mythread_startup, stack + stack_size,
-                    CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND |
-                        CLONE_THREAD | CLONE_SYSVSEM,
-                    (void *)thread);
+  stack = (void *)thread;
+
+  int child_pid = clone(mythread_startup, stack,
+                        CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND |
+                            CLONE_THREAD | CLONE_SYSVSEM,
+                        (void *)thread);
   if (child_pid == -1) {
-    printf("Clone is failed, %s!\n", strerror(errno));
-    return 1;
+    destroy_thread(thread);
+    return errno;
   }
 
-  tid = thread;
+  *tid = thread;
 
   return 0;
 }
