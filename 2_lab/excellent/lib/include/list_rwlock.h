78236418c3fd81b5c97fbb54cef0d9b7ebafc2bf
mashawarface <sony.sky.16@gmail.com>
2025-12-02 17:50:21 +0700

    added rwlock version

---
+++ 2_lab/excellent/lib/include/list_rwlock.h
@@ -0,0 +1,36 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <pthread.h>
+#include <time.h>
+
+#define MAX_LENGTH 10
+
+typedef struct node {
+  char buf[MAX_LENGTH];
+  struct node *next;
+  pthread_rwlock_t sync;
+} node_t;
+
+typedef struct list {
+  node_t *first;
+  pthread_rwlock_t sync;
+} list_t;
+
+typedef struct search_args {
+  list_t *list;
+  size_t *counter;
+  int (*compare)(int curr, int prev);
+} search_thread_arg_t;
+
+typedef struct swap_args {
+  list_t *list;
+  size_t *counter;
+} swap_thread_arg_t;
+
+list_t *list_init(size_t size);
+void list_destroy(list_t *list);
+void print_list(list_t *list);
+
+
+#endif // QUEUE_H
